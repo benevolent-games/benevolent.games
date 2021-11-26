98dd51eeae9224b64827772db038bcc89b632412
Chase Moskal <chasemoskal@gmail.com>
2021-11-25 21:01:12 -0800

    add: draco compression, maybe?

--- s/demo/setup-babylon-side-effects.ts
+++ s/demo/setup-babylon-side-effects.ts
@@ -1,7 +1,7 @@
 
-import "@babylonjs/core"
+import "@babylonjs/core/index.js"
 import "@babylonjs/core/Meshes/meshBuilder.js"
-import "@babylonjs/loaders/glTF/2.0/glTFLoader.js"
+import "@babylonjs/loaders/glTF/2.0/Extensions/KHR_draco_mesh_compression.js"
 
 import {SceneLoader} from "@babylonjs/core/Loading/sceneLoader.js"
 
