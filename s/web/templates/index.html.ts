6e767aac2158a54a0dbee23f5ecae2bdf60e02c6
Chase Moskal <chasemoskal@gmail.com>
2021-11-25 19:26:32 -0800

    remove: old html and broken webmanifest

--- s/web/templates/index.html.ts
+++ s/web/templates/index.html.ts
@@ -17,7 +17,6 @@ export default ({debug}: {debug: boolean}) => html`
 	<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png"/>
 	<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png"/>
 	<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png"/>
-	<link rel="manifest" href="/assets/favicon/site.webmanifest"/>
 </head>
 <body>
 	<main style="display: none">
--- s/web/templates/playground.html.ts
+++ s/web/templates/playground.html.ts
@@ -44,7 +44,6 @@ export default ({debug}: {debug: boolean}) => html`
 	<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png"/>
 	<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png"/>
 	<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png"/>
-	<link rel="manifest" href="/assets/favicon/site.webmanifest"/>
 
 	<script type=module defer src=${debug ? "axiom.bundle.js" : "axiom.bundle.min.js"}></script>
 </head>
