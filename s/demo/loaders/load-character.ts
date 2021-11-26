21cfee04732aea9d18cbea5a5da8478902d2e31c
Chase Moskal <chasemoskal@gmail.com>
2021-11-25 20:19:48 -0800

    add: character 3

--- s/demo/loaders/load-character.ts
+++ s/demo/loaders/load-character.ts
@@ -3,7 +3,7 @@ import {Scene} from "@babylonjs/core/scene.js"
 import {SceneLoader} from "@babylonjs/core/Loading/sceneLoader.js"
 
 export async function loadCharacter(scene: Scene) {
-	await SceneLoader.AppendAsync("/assets/", "character-02.glb", scene, () => {
+	await SceneLoader.AppendAsync("/assets/", "character-03.glb", scene, () => {
 		console.log("character loaded")
 	})
 }
