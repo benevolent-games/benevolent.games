a39def11ff7ddd11c7bdc3d9afea693b47f0c8c4
Chase Moskal <chasemoskal@gmail.com>
2021-11-26 01:08:27 -0800

    enable: all gltf extensions

--- s/demo/setup-babylon-side-effects.ts
+++ s/demo/setup-babylon-side-effects.ts
@@ -1,7 +1,7 @@
 
 import "@babylonjs/core/index.js"
 import "@babylonjs/core/Meshes/meshBuilder.js"
-import "@babylonjs/loaders/glTF/2.0/Extensions/KHR_draco_mesh_compression.js"
+import "@babylonjs/loaders/glTF/2.0/Extensions/index.js"
 
 import {SceneLoader} from "@babylonjs/core/Loading/sceneLoader.js"
 
