f01eb60f11cd60a903888dfcd9ded0f219478a35
Chase Moskal <chasemoskal@gmail.com>
2021-11-25 22:36:12 -0800

    add: animated character 4

--- s/demo/loaders/load-character.ts
+++ s/demo/loaders/load-character.ts
@@ -3,7 +3,23 @@ import {Scene} from "@babylonjs/core/scene.js"
 import {SceneLoader} from "@babylonjs/core/Loading/sceneLoader.js"
 
 export async function loadCharacter(scene: Scene) {
-	await SceneLoader.AppendAsync("/assets/", "character-03.glb", scene, () => {
-		console.log("character loaded")
-	})
+
+	await SceneLoader.AppendAsync("/assets/", "character-04.glb", scene, () => {})
+	console.log("character loaded")
+
+	const man = scene.meshes.find(m => m.id.startsWith("man"))
+	if (!man)
+		throw new Error("man not found")
+
+	let index = 0
+	const animationGroups = scene.animationGroups
+
+	setInterval(() => {
+		scene.stopAllAnimations()
+		const animationGroup = animationGroups[index]
+		animationGroup.start(true)
+		index += 1
+		if (index >= animationGroups.length)
+			index = 0
+	}, 2 * 1000)
 }
