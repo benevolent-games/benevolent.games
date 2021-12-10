f0e9e93d7d0d56ae3e7157ac74aed4a7b33b6159
Chase Moskal <chasemoskal@gmail.com>
2021-12-09 20:46:41 -0800

    rework: package build, and add heartbeat area

--- s/web/build.ts
+++ s/web/build.ts
@@ -4,6 +4,7 @@ import {prepareTransformer} from "./utils/prepare-transformer.js"
 import styleCss from "./templates/style.css.js"
 import playHtml from "./templates/play.html.js"
 import indexHtml from "./templates/index.html.js"
+import heartbeatHtml from "./templates/heartbeat.html.js"
 
 const options = {
 	debug: process.argv.includes("debug")
@@ -13,3 +14,4 @@ const transform = prepareTransformer("./x/")
 await transform("style.css", styleCss())
 await transform("index.html", indexHtml(options))
 await transform("play.html", playHtml(options))
+await transform("heartbeat.html", heartbeatHtml(options))
---
+++ s/web/templates/heartbeat.html.ts
@@ -0,0 +1,21 @@
+
+import headBasicsHtml from "../partials/head-basics.html.js"
+import {noop as html} from "../utils/template-noop.js"
+
+export default ({debug}: {debug: boolean}) => html`
+
+<!doctype html>
+<html>
+<head>
+	${headBasicsHtml({title: "axiom 💙"})}
+	<script defer type=module-shim src="/heart/heartbeat-demo.js"></script>
+	<script defer type=importmap-shim src="/importmap.json"></script>
+	<script defer src="/node_modules/es-module-shims/dist/es-module-shims.js"></script>
+</head>
+<body class=heartbeat>
+	<h1>heartbeat network tests</h1>
+	<div class=app></div>
+</body>
+</html>
+
+`
