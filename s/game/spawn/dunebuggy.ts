e150b7dcf775c8f21b217516558a87674a14c069
Chase Moskal <chasemoskal@gmail.com>
2022-01-24 10:27:31 -0800

    rename: dunebuggy files

--- s/game/spawn/dunebuggy.ts
+++ s/game/spawn/dunebuggy.ts
@@ -5,7 +5,7 @@ import {loadGlb} from "../babylon/load-glb.js"
 
 export function spawnDunebuggy({scene}: SpawnOptions) {
 	return async function(position: V3) {
-		const assets = await loadGlb(scene, "/assets/models/dunebuggy/dunebuggy.poo.glb")
+		const assets = await loadGlb(scene, "/assets/models/dunebuggy/dunebuggy.lod1.glb")
 		assets.removeAllFromScene()
 		assets.addAllToScene()
 		// mesh.position = new BABYLON.Vector3(...position)
