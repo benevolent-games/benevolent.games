98a9bc892ea3e9c9c420b7baab670e4ff3329055
Chase Moskal <chasemoskal@gmail.com>
2022-02-04 03:03:20 -0800

    delete: heartbeat demo

--- s/web/build.ts
+++ s/web/build.ts
@@ -4,7 +4,6 @@ import {prepareTransformer} from "./utils/prepare-transformer.js"
 import styleCss from "./templates/style.css.js"
 import indexHtml from "./templates/index.html.js"
 import humanoidHtml from "./templates/humanoid.html.js"
-import heartbeatHtml from "./templates/heartbeat.html.js"
 import thumbHtml from "./templates/thumb.html.js"
 
 const options = {
@@ -16,4 +15,3 @@ await transform("style.css", styleCss())
 await transform("index.html", indexHtml(options))
 await transform("thumb.html", thumbHtml(options))
 await transform("humanoid.html", humanoidHtml(options))
-await transform("heartbeat.html", heartbeatHtml(options))
