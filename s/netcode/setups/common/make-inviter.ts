dd0703ea759a29b6b6c1d5669f6c8a436edb29ff
Chase Moskal <chasemoskal@gmail.com>
2022-02-06 19:10:03 -0800

    improve: invite link copy compatibility

--- s/netcode/setups/common/make-inviter.ts
+++ s/netcode/setups/common/make-inviter.ts
@@ -8,12 +8,28 @@ export function makeInviter(state: ReturnType<typeof makeNetworkingState>) {
 	const hideInviteCopiedIndicator = debounce(2000, () => {
 		state.writable.inviteCopied = false
 	})
-	return function() {
+	return async function() {
 		const {sessionId} = state.readable
-		state.writable.inviteCopied = true
 		const link = sessionLink(location.href, sessionTerm, sessionId)
-		console.log(link)
-		navigator.clipboard.writeText(link)
-		hideInviteCopiedIndicator()
+		console.log("invite link", link)
+
+		async function attemptCopy(text: string) {
+			try {
+				await navigator.clipboard.writeText(text)
+				state.writable.inviteCopied = true
+			}
+			finally {
+				hideInviteCopiedIndicator()
+			}
+		}
+
+		if (navigator.permissions) {
+			const permission = "clipboard-write" as PermissionName
+			const result = await navigator.permissions.query({name: permission})
+			if (result.state == "granted" || result.state == "prompt")
+				attemptCopy(link)
+		}
+		else
+			attemptCopy(link)
 	}
 }
