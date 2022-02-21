00234fbcce3e21bd9c7733bda94e250659615270
Chase Moskal <chasemoskal@gmail.com>
2022-02-20 22:11:44 -0800

    implement: crate client interpolation

--- s/game/spawn/crate.ts
+++ s/game/spawn/crate.ts
@@ -3,13 +3,22 @@ import * as v3 from "../utils/v3.js"
 import * as quat from "../utils/quat.js"
 import {asEntity, CrateDescription, Spawner, SpawnOptions} from "../types.js"
 
+const interpolationSteps = 3
+
 export function spawnCrate({scene}: SpawnOptions): Spawner<CrateDescription> {
-	return async function({host}) {
+	return async function({host, description}) {
+
+		const disposers = new Set<() => void>()
+		const goalposts = {
+			position: description.position,
+			rotation: description.rotation ?? quat.zero(),
+		}
 
 		const mesh = BABYLON.MeshBuilder.CreateBox("crate", {size: 1}, scene)
 		const material = new BABYLON.StandardMaterial("cratemat", scene)
 		material.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5)
 		mesh.material = material
+		disposers.add(() => mesh.dispose())
 
 		if (host) {
 			mesh.physicsImpostor = new BABYLON.PhysicsImpostor(
@@ -21,16 +30,35 @@ export function spawnCrate({scene}: SpawnOptions): Spawner<CrateDescription> {
 					restitution: 0.5,
 				}
 			)
+			mesh.position = v3.toBabylon(description.position)
 		}
 		else {
-			material.alpha = 0.5
+			function physicsCallback() {
+				const currentPosition = v3.fromBabylon(mesh.position)
+				const difference = v3.subtract(goalposts.position, currentPosition)
+				const step = v3.divideBy(difference, interpolationSteps)
+				const newPosition = v3.add(currentPosition, step)
+				mesh.position = v3.toBabylon(newPosition)
+
+				{
+					const goalRotation = quat.toBabylon(goalposts.rotation)
+					const newRotation = BABYLON.Quaternion.Slerp(
+						mesh.rotationQuaternion ?? BABYLON.Quaternion.Zero(),
+						goalRotation,
+						1 / interpolationSteps,
+					)
+					mesh.rotationQuaternion = newRotation
+				}
+			}
+			scene.onBeforePhysicsObservable.add(physicsCallback)
+			disposers.add(() => scene.onBeforePhysicsObservable.removeCallback(physicsCallback))
 		}
 
 		return asEntity<CrateDescription>({
 			update: description => {
-				mesh.position = v3.toBabylon(description.position)
+				goalposts.position = description.position
 				if (description.rotation)
-					mesh.rotationQuaternion = quat.toBabylon(description.rotation)
+					goalposts.rotation = description.rotation
 			},
 			describe: () => ({
 				type: "crate",
@@ -38,7 +66,8 @@ export function spawnCrate({scene}: SpawnOptions): Spawner<CrateDescription> {
 				rotation: quat.fromBabylon(mesh.rotationQuaternion),
 			}),
 			dispose() {
-				mesh.dispose()
+				for (const dispose of disposers)
+					dispose()
 			},
 			receiveMemo() {},
 		})
--- s/game/utils/quat.ts
+++ s/game/utils/quat.ts
@@ -1,6 +1,10 @@
 
 export type Quat = [number, number, number, number]
 
+export function zero(): Quat {
+	return [0, 0, 0, 0]
+}
+
 export function toBabylon([x, y, z, w]: Quat) {
 	return new BABYLON.Quaternion(x, y, z, w)
 }
